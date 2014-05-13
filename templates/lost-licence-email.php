701ae01eb20c9f2b698f8e2137638d875cef5fcb
Mike Jolley <mike.jolley@me.com>
2014-05-13 16:36:26 +0100

    Plain text email template

--- templates/lost-licence-email.php
+++ templates/lost-licence-email.php
@@ -2,31 +2,28 @@
 /**
  * Lost licence email
  */
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+_e( "Hello there,", 'wp-plugin-licencing' );
+echo "\n";
+echo "\n";
+_e( "Your licence keys are as follows:", 'wp-plugin-licencing' );
+echo "\n";
+
+foreach ( $keys as $key ) {
+	$api_product = wppl_get_licence_product( $key->product_id );
+
+	echo "\n====================\n";
+	echo esc_html( $api_product->post_title ) . ': ' . wppl_get_package_download_url( $api_product_permission, $key->licence_key, $key->activation_email ) . "\n";
+	echo $key->licence_key . "";
 }
-?>
 
-<?php do_action( 'woocommerce_email_header', $email_heading ); ?>
+echo "\n====================\n\n";
 
-<p><?php _e( "Your licence keys are as follows:", 'wp-plugin-licencing' ); ?></p>
+_e( "You can input these on the plugins page within your WordPress dashboard.", 'wp-plugin-licencing' );
+echo "\n";
+echo "\n";
 
-<ul>
-	<?php foreach ( $keys as $key ) : $api_product = wppl_get_licence_product( $key->product_id ); ?>
-		<li>
-			<?php echo esc_html( $api_product->post_title ); ?>: <strong><?php echo $key->licence_key; ?></strong>
-			<?php
-				if ( $api_product_permissions = wppl_get_licence_api_product_permissions( $key->product_id ) ) {
-					echo '<ul class="digital-downloads">';
-					foreach ( $api_product_permissions as $api_product_permission ) {
-						echo '<li><a class="download-button" href="' . wppl_get_package_download_url( $api_product_permission, $key->licence_key, $key->activation_email ) . '">' . sprintf( __( 'Download %s', 'wp-plugin-licencing' ), get_the_title( $api_product_permission ) ) . '</a></li>';
-					}
-					echo '</ul>';
-				}
-			?>
-		</li>
-	<?php endforeach; ?>
-</ul>
-
-<?php do_action( 'woocommerce_email_footer' ); ?>
+// Footer
+echo '--' . "\n";
+echo apply_filters( 'woocommerce_email_footer_text', get_option( 'woocommerce_email_footer_text' ) );
