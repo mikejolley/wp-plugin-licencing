89881b5f3e65dc5d25ed7dd1d53836cccac03102
Barry Kooij <b.kooij@gmail.com>
2014-10-12 20:08:38 +0200

    Fix notices of unset placeholders.

--- includes/class-wp-plugin-licencing-post-types.php
+++ includes/class-wp-plugin-licencing-post-types.php
@@ -240,53 +240,60 @@ class WP_Plugin_Licencing_Post_Types {
 	public function input_file( $key, $field ) {
 		global $thepostid;
 
-		if ( empty( $field['value'] ) )
+		if ( empty( $field['value'] ) ) {
 			$field['value'] = get_post_meta( $thepostid, $key, true );
+		}
+
+		if ( ! isset( $field['placeholder'] ) ) {
+			$field['placeholder'] = '';
+		}
+
 		?>
 		<p class="form-field">
-			<label for="<?php echo esc_attr( $key ); ?>"><?php echo esc_html( $field['label'] ) ; ?>:</label>
+			<label for="<?php echo esc_attr( $key ); ?>"><?php echo esc_html( $field['label'] ); ?>:</label>
 			<input type="text" class="file_url" name="<?php echo esc_attr( $key ); ?>" id="<?php echo esc_attr( $key ); ?>" placeholder="<?php echo esc_attr( $field['placeholder'] ); ?>" value="<?php echo esc_attr( $field['value'] ); ?>" />
-			<button class="button upload_image_button" data-uploader_button_text="<?php _e( 'Use file', 'wp-plugin-licencing' ); ?>"><?php _e( 'Upload', 'wp-plugin-licencing' ); ?></button> <?php if ( ! empty( $field['description'] ) ) : ?><span class="description"><?php echo $field['description']; ?></span><?php endif; ?>
+			<button class="button upload_image_button" data-uploader_button_text="<?php _e( 'Use file', 'wp-plugin-licencing' ); ?>"><?php _e( 'Upload', 'wp-plugin-licencing' ); ?></button> <?php if ( ! empty( $field['description'] ) ) : ?>
+				<span class="description"><?php echo $field['description']; ?></span><?php endif; ?>
 		</p>
 		<script type="text/javascript">
 			// Uploading files
 			var file_frame;
 			var file_target_input;
 
-			jQuery('.upload_image_button').live('click', function( event ){
+			jQuery('.upload_image_button').live('click', function (event) {
 
-			    event.preventDefault();
+				event.preventDefault();
 
-			    file_target_input = jQuery( this ).closest('.form-field').find('.file_url');
+				file_target_input = jQuery(this).closest('.form-field').find('.file_url');
 
-			    // If the media frame already exists, reopen it.
-			    if ( file_frame ) {
+				// If the media frame already exists, reopen it.
+				if (file_frame) {
 					file_frame.open();
 					return;
-			    }
+				}
 
-			    // Create the media frame.
-			    file_frame = wp.media.frames.file_frame = wp.media({
-					title: jQuery( this ).data( 'uploader_title' ),
-					button: {
-						text: jQuery( this ).data( 'uploader_button_text' ),
+				// Create the media frame.
+				file_frame = wp.media.frames.file_frame = wp.media({
+					title   : jQuery(this).data('uploader_title'),
+					button  : {
+						text: jQuery(this).data('uploader_button_text'),
 					},
 					multiple: false  // Set to true to allow multiple files to be selected
-			    });
+				});
 
-			    // When an image is selected, run a callback.
-			    file_frame.on( 'select', function() {
+				// When an image is selected, run a callback.
+				file_frame.on('select', function () {
 					// We set multiple to false so only get one image from the uploader
 					attachment = file_frame.state().get('selection').first().toJSON();
 
-					jQuery( file_target_input ).val( attachment.url );
-			    });
+					jQuery(file_target_input).val(attachment.url);
+				});
 
-			    // Finally, open the modal
-			    file_frame.open();
+				// Finally, open the modal
+				file_frame.open();
 			});
 		</script>
-		<?php
+	<?php
 	}
 
 	/**
@@ -298,15 +305,22 @@ class WP_Plugin_Licencing_Post_Types {
 	public function input_text( $key, $field ) {
 		global $thepostid;
 
-		if ( empty( $field['value'] ) )
+		if ( empty( $field['value'] ) ) {
 			$field['value'] = get_post_meta( $thepostid, $key, true );
+		}
+
+		if ( ! isset( $field['placeholder'] ) ) {
+			$field['placeholder'] = '';
+		}
+
 		?>
 		<p class="form-field">
-			<label for="<?php echo esc_attr( $key ); ?>"><?php echo esc_html( $field['label'] ) ; ?>:</label>
+			<label for="<?php echo esc_attr( $key ); ?>"><?php echo esc_html( $field['label'] ); ?>:</label>
 			<input type="text" name="<?php echo esc_attr( $key ); ?>" id="<?php echo esc_attr( $key ); ?>" placeholder="<?php echo esc_attr( $field['placeholder'] ); ?>" value="<?php echo esc_attr( $field['value'] ); ?>" />
-			<?php if ( ! empty( $field['description'] ) ) : ?><span class="description"><?php echo $field['description']; ?></span><?php endif; ?>
+			<?php if ( ! empty( $field['description'] ) ) : ?>
+				<span class="description"><?php echo $field['description']; ?></span><?php endif; ?>
 		</p>
-		<?php
+	<?php
 	}
 
 	/**
@@ -321,15 +335,20 @@ class WP_Plugin_Licencing_Post_Types {
 		if ( empty( $field['value'] ) ) {
 			$field['value'] = get_post_meta( $thepostid, $key, true );
 		}
+
+		if ( ! isset( $field['placeholder'] ) ) {
+			$field['placeholder'] = '';
+		}
 		?>
 		<p class="form-field">
-			<label for="<?php echo esc_attr( $key ); ?>"><?php echo esc_html( $field['label'] ) ; ?>:</label>
+			<label for="<?php echo esc_attr( $key ); ?>"><?php echo esc_html( $field['label'] ); ?>:</label>
 			<textarea name="<?php echo esc_attr( $key ); ?>" id="<?php echo esc_attr( $key ); ?>" placeholder="<?php echo esc_attr( $field['placeholder'] ); ?>" cols="25" rows="3"><?php echo esc_html( $field['value'] ); ?></textarea>
-			<?php if ( ! empty( $field['description'] ) ) : ?><span class="description"><?php echo $field['description']; ?></span><?php endif; ?>
+			<?php if ( ! empty( $field['description'] ) ) : ?>
+				<span class="description"><?php echo $field['description']; ?></span><?php endif; ?>
 		</p>
-		<?php
+	<?php
 	}
-	
+
 	/**
 	 * input_select function.
 	 *
