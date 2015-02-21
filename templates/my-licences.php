fcb03ab31c76fc53b32cd2758210d26b3ebac43a
Mike Jolley <mike.jolley@me.com>
2015-02-21 13:47:33 +0000

    Timestamp Closes #1

--- templates/my-licences.php
+++ templates/my-licences.php
@@ -17,6 +17,9 @@ if ( sizeof( $keys ) > 0 ) : ?>
 			<?php foreach ( $keys as $key ) :
 				$product     = wppl_get_licence_product( $key->product_id );
 				$activations = $wpdb->get_results( $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}wp_plugin_licencing_activations WHERE activation_active = 1 AND licence_key=%s;", $key->licence_key ) );
+
+				// The expire time stamp, will be false if no expire date is set
+				$expire_ts   = strtotime( $key->date_expires );
 				?>
 				<tr>
 					<td rowspan="<?php echo sizeof( $activations ) + 1; ?>"><?php echo esc_html( $product->post_title ); ?></td>
@@ -24,14 +27,14 @@ if ( sizeof( $keys ) > 0 ) : ?>
 						<code style="display:block;"><?php echo $key->licence_key; ?></code>
 						<small>
 							<?php printf( __( 'Activation email: %s.', 'wp-plugin-licencing' ), $key->activation_email ); ?>
-							<?php if ( $key->date_expires ) : ?>
+							<?php if ( false !== $expire_ts && $expire_ts > 0 ) : ?>
 								<?php printf( __( 'Expiry date: %s.', 'wp-plugin-licencing' ), date_i18n( get_option( 'date_format' ), strtotime( $key->date_expires ) ) ); ?>
 							<?php endif; ?>
 						</small>
 					</td>
 					<td><?php echo $key->activation_limit ? sprintf( __( '%d per product', 'wp-plugin-licencing' ), absint( $key->activation_limit ) ) : __( 'Unlimited', 'wp-plugin-licencing' ); ?></td>
 					<td><?php
-						if ( $key->date_expires && strtotime( $key->date_expires ) < current_time( 'timestamp' ) ) {
+						if ( $key->date_expires && false !== $expire_ts && $expire_ts > 0 && $expire_ts < current_time( 'timestamp' ) ) {
 							echo '<a class="button" href="' . wppl_get_licence_renewal_url( $key->licence_key, $key->activation_email ) . '">' . __( 'Renew licence', 'wp-plugin-licencing' ) . '</a>';
 						} else {
 							if ( $api_product_permissions = wppl_get_licence_api_product_permissions( $key->product_id ) ) {
